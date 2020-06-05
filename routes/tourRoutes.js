0fdc6a33ba4a1ea1f3ad71d214ab0c40fbe74b8e
Achraf Elhamzaoui <achrfelh22@gmail.com>
2020-06-05 10:34:08 +0000

    Finish initial Backend req and res with express of the app

---
+++ routes/tourRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const { getAllTours, createTour, getTour, updateTour, deleteTour, checkId, checkBody } = require("./../controllers/tourController");
+
+const router = express.Router();
+
+router.param("id", checkId);
+
+
+router.route('/').get(getAllTours).post(checkBody, createTour);
+router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+module.exports = router
