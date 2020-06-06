725085975addcf72c6e757ae50967c854a2739c2
Achraf Elhamzaoui <achrfelh22@gmail.com>
2020-06-06 10:55:15 +0000

    Init The database with Mongoose

--- serveur.js
+++ serveur.js
@@ -1,7 +1,51 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+// Connect to the DB
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection succeffuly');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const tourTest = new Tour({
+  name: 'The Parker Camper',
+  price: 297,
+});
+
+tourTest
+  .save()
+  .then((doc) => console.log(doc))
+  .catch((err) => console.log(`Error 💣💣 ${err} `));
+
+// Serveur
 
 const port = process.env.PORT || 3000;
 
