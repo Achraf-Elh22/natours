ba863440770b186883d8b7506acad86e07f72cd9
Achraf <achrafelh22@gmail.com>
2020-06-11 13:38:26 +0100

    Build alias For the Top cheapest five tours

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,13 +1,21 @@
 const express = require('express');
 
-const { getAllTours, createTour, getTour, updateTour, deleteTour } = require("../controllers/tourController");
+const {
+  getAllTours,
+  createTour,
+  getTour,
+  updateTour,
+  deleteTour,
+  aliasTopTours,
+} = require('../controllers/tourController');
 
 const router = express.Router();
 
 // router.param("id", checkId);
 
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
-module.exports = router
+module.exports = router;
