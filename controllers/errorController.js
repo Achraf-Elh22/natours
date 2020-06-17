ad59efe26255ba3463665845ce95b90bcc20d082
Achraf <achrafelh22@gmail.com>
2020-06-17 12:12:24 +0100

    Handling Error During the development and production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,39 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //Operationel, trusted Error: Send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  // Programming or other unknown Error: don't leak error details
+  else {
+    // 1)log Error
+    console.error('ERROR 💣 ', err);
+    // 2) Send Generate message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something Went very Wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
--- utils/appError.js
+++ utils/appError.js
@@ -1,10 +1,9 @@
 class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
-
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith(4) ? 'Fail' : 'Error';
-    this.isOperational = true; // Ex: When User Left required input empty
+    this.isOperational = true; // Ex: When User Left required input empty // IN GENERAL ITS THE USER FAULT
 
     Error.captureStackTrace(this, this.constructor);
   }
