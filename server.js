cefd0db9d8239f45446abc72934286edb48d4da0
Achraf <achrafelh22@gmail.com>
2020-06-19 10:47:47 +0100

    Handle uncaught exception

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION!! 💣💣 Shunting down .... ');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -32,3 +38,5 @@ process.on('unhandledRejection', (err) => {
     process.exit();
   });
 });
+
+console.log(x);
