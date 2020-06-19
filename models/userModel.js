8519acca206d9bb021334f82e9fe68556d4004e3
Achraf <achrafelh22@gmail.com>
2020-06-19 11:55:43 +0100

    Create authController and handling the basics of signUp and fix missing fields in User Schema

--- models/userModel.js
+++ models/userModel.js
@@ -9,6 +9,8 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Please provide your email!'],
+    unique: true,
+    lowercase: true,
     validate: [validator.isEmail, 'Please provide a valid email'],
   },
   photo: {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,17 @@
 const express = require('express');
 
-const { getAllUsers, createUser, getUser, updateUser, deleteUser } = require("./../controllers/userController");
+const {
+  getAllUsers,
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require('../controllers/userController');
+const { signUp } = require('../controllers/authController');
 
 const router = express.Router();
+
+router.route('/signup').post(signUp);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
