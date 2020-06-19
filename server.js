bad24a1553bce3ee2a31255df154f39eb5d6ce83
Achraf <achrafelh22@gmail.com>
2020-06-19 10:36:16 +0100

    Handle Express unhandled Rejections

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ mongoose
 // Serveur
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION!!! 💣💣 Shunting down .... ');
+  server.close(() => {
+    process.exit();
+  });
 });
