a33310b8137405cf4a2811904fa38f7e8667af0e
Achraf <achrafelh22@gmail.com>
2020-07-10 12:13:00 +0100

    Adding missing authentication and Authorization to the ROUTES

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -15,10 +15,17 @@ const { protect, restrictTo } = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
+router.use(protect);
+
 router
   .route('/')
   .get(getAllReviews)
   .post(protect, restrictTo('user'), setTourUserIds, createReview);
-router.route('/:id').get(getReview).patch(updateReview).delete(deleteReview);
+
+router
+  .route('/:id')
+  .get(getReview)
+  .patch(restrictTo('user', 'admin'), updateReview)
+  .delete(restrictTo('user', 'admin'), deleteReview);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,6 +8,7 @@ const {
   deleteUser,
   updateMe,
   deleteMe,
+  getMe,
 } = require('../controllers/userController');
 const {
   signUp,
@@ -16,19 +17,25 @@ const {
   resetPassword,
   updatePassword,
   protect,
+  restrictTo,
 } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.route('/signup').post(signUp);
 router.route('/login').post(login);
-router.route('/updatePassword').patch(protect, updatePassword);
-
-router.route('/updateMe').patch(protect, updateMe);
-router.route('/deleteMe').delete(protect, deleteMe);
-
 router.route('/forgotPassword').post(forgotPassword);
 router.route('/resetPassword/:token').patch(resetPassword);
+
+// Protect all routes after this middleware
+router.use(protect);
+
+router.route('/updatePassword').patch(updatePassword);
+router.route('/me').get(getMe, getUser);
+router.route('/updateMe').patch(updateMe);
+router.route('/deleteMe').delete(deleteMe);
+
+router.use(restrictTo('admin'));
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
