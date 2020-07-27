dd83d7ffcd60b37da2f6fe06d0b4a7f6f66ea96a
Achraf <achrafelh22@gmail.com>
2020-07-27 09:59:47 +0100

    Buid Rendering error Page

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,32 +1,56 @@
 const AppError = require('../utils/appError');
 
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-    error: err,
-    stack: err.stack,
+const sendErrorDev = (err, req, res) => {
+  // API
+  if (req.originalUrl.startsWith('/api')) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+      error: err,
+      stack: err.stack,
+    });
+  }
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong!!',
+    msg: err.message,
   });
 };
 
-const sendErrorProd = (err, res) => {
-  //Operationel, trusted Error: Send message to client
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
-  }
-  // Programming or other unknown Error: don't leak error details
-  else {
+const sendErrorProd = (err, req, res) => {
+  // A) API
+  if (req.originalUrl.startsWith('/api')) {
+    //Operationel, trusted Error: Send message to client
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    }
+    // Programming or other unknown Error: don't leak error details
     // 1)log Error
     console.error('ERROR 💣 ', err);
     // 2) Send Generate message
-    res.status(500).json({
+    return res.status(err.statusCode).json({
       status: 'error',
-      message: 'Something Went very Wrong',
+      message: 'Something went wrong!!',
     });
   }
+  // B) Rendered website
+  //Operationel, trusted Error: Send message to client
+  if (err.isOperational) {
+    return res.status(err.statusCode).render('error', {
+      title: 'Something went wrong!!',
+      msg: err.message,
+    });
+  }
+  // 1)log Error
+  console.error('ERROR 💣 ', err);
+
+  // 2) Send Generate message
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong!!',
+    msg: 'Please try again later',
+  });
 };
 
 const handleCastErrorDB = (err) => {
@@ -58,13 +82,13 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
+    sendErrorDev(err, req, res);
   } else {
     if (err.name === 'CastError') err = handleCastErrorDB(err);
     if (err.code === 11000) err = handleDuplicateFieldsDB(err);
     if (err.name === 'ValidationError') err = handleValidationError(err);
     if (err.name === 'JsonWebTokenError') err = handleJWTError();
     if (err.name === 'TokenExpiredError') err = handleJWTExpiredError();
-    sendErrorProd(err, res);
+    sendErrorProd(err, req, res);
   }
 };
--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res) => {
   const tours = await Tour.find();
@@ -9,11 +10,15 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user',
   });
+
+  // if (!tour) {
+  //   return next(new AppError('There is no Tour with that name', 404));
+  // }
 
   res.status(200).render('tour', {
     title: `${tour.name} Tour`,
