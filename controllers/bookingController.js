a1c47b93962f8d78cab758bf097054a3940f72d2
Achraf Elhamzaoui <achrfelh22@gmail.com>
2020-08-16 11:45:50 +0100

    Fix a dumb bug type error

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -34,24 +34,20 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.webhookCheckout = (req,res,next){
+exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
 
   let event;
   try {
-    event = stripe.webhooks.constructEvent(
-      req.body,
-      signature,
-      process.env.STRIPE_WEBHOOK_SECRET
-    );
+    event = stripe.webhooks.constructEvent(req.body, signature, process.env.STRIPE_WEBHOOK_SECRET);
   } catch (err) {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
 
-  if (event.type === 'checkout.session.completed')
-    createBookingCheckout(event.data.object);
+  if (event.type === 'checkout.session.completed') createBookingCheckout(event.data.object);
 
-  res.status(200).json({ received: true });}
+  res.status(200).json({ received: true });
+};
 
 exports.getAllBookings = factory.getAll(Booking);
 exports.getBooking = factory.getOne(Booking);
